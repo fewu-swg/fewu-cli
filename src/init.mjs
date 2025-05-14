c1013ab1ee119be19091a329d04b786614ce7726
0xarch <alanqa-ops@outlook.com>
2025-05-14 22:02:30 +0800

    support flexible package.json

--- src/init.mjs
+++ src/init.mjs
@@ -2,7 +2,6 @@ import { join } from 'path';
 import { existsSync } from 'fs';
 import { writeFile, mkdir } from 'fs/promises';
 import { value as config } from './common/config.yaml.mjs';
-import { value as packageJson } from './common/package.json.mjs';
 import { guide_content, value as postTemplate } from './common/template.mjs';
 
 const app = ({
@@ -15,6 +14,26 @@ async function safeWriteFile(path, data) {
     else await writeFile(path, data);
 }
 
+async function _package_json(){
+    const dependencies = {
+        "@fewu-swg/fewu-renderer-markdown": "^1.2.2",
+        "@fewu-swg/fewu-theme-next": "^1.0.2"
+    };
+    const package_json_path = join(process.cwd(),"package.json");
+    let target_package_json = {
+        dependencies: {}
+    };
+    if(existsSync(package_json_path)){
+        let current_package_json = JSON.parse((await readFile(package_json_path)).toStirng());
+        target_package_json = current_package_json;
+    }
+    target_package_json.dependencies = {
+        ...target_package_json.dependencies,
+        ...dependencies
+    };
+    await writeFile(package_json_path,JSON.stringify(target_package_json,void 0,4));
+}
+
 async function _local() {
     const requiredDirectories = ['source/posts/guide', 'source/drafts', 'public', 'themes'];
     for (let directory of requiredDirectories) {
@@ -25,7 +44,8 @@ async function _local() {
         safeWriteFile(join('source/posts/guide', 'guide.zh-CN.md'), guide_content.cn),
         safeWriteFile(join('source/posts/guide', 'guide.en-US.md'), guide_content.en),
         safeWriteFile('./config.yaml', config()),
-        safeWriteFile('./package.json', packageJson()),
+        // safeWriteFile('./package.json', packageJson()),
+        _package_json()
     ]);
     console.log(`Local initialization complete. Check source, public, themes folder and config.yaml.`);
     console.log(`You can clone other themes into themes/, make sure the required dependencies are installed.`);
