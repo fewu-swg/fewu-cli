d07afb7a1ec693697c12478453d54bd9bc34ea4a
0xarch <alanqa-ops@outlook.com>
2025-06-17 14:23:52 +0800

    follow fewu 3.5.0

--- src/common/package.json.mjs
+++ src/common/package.json.mjs
@@ -1,6 +1,6 @@
 export const value = _ => `{
     "dependencies": {
-        "@fewu-swg/fewu-theme-next": "^1.0.0",
+        "@fewu-swg/fewu-theme-next": "^1.1.0,
         "@fewu-swg/fewu-renderer-markdown": "^1.2.2"
     }
 }`
